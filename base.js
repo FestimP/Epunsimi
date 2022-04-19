508fca29cb28182c75322028751be4e59f3e17a4
FestimsRepo <102750865+FestimP@users.noreply.github.com>
2022-04-19 21:29:22 +0200

    Squashed commit of the following:
    commit e567c6f179d6d07277b9ddc7a5eb5bb37552676f
    Author: FestimsRepo <102750865+FestimP@users.noreply.github.com>
    Date:   Tue Apr 19 11:14:34 2022 +0200
    
        pop
    
    commit 29fca88ae1949bcf3810ccd0b91ae74b6d3bd481
    Author: FestimsRepo <102750865+FestimP@users.noreply.github.com>
    Date:   Tue Apr 19 11:08:42 2022 +0200
    
        susu
    
    commit c59855b7c186562758421ba99ef59a3d46405e63
    Author: FestimsRepo <102750865+FestimP@users.noreply.github.com>
    Date:   Tue Apr 19 11:00:00 2022 +0200
    
        kpopin
    
    commit 24438c1b2dd7e085780697ef60406377b786f159
    Author: FestimsRepo <102750865+FestimP@users.noreply.github.com>
    Date:   Tue Apr 19 02:02:11 2022 +0200
    
        ..
    
    commit 85ce79dc8651ecc845b225893c6212e016ac0f1e
    Author: FestimsRepo <102750865+FestimP@users.noreply.github.com>
    Date:   Tue Apr 19 01:39:55 2022 +0200
    
        .
    
    commit fd94d3f40720c49754c941e4b52c4c1fa69b91ba
    Author: FestimsRepo <102750865+FestimP@users.noreply.github.com>
    Date:   Tue Apr 19 01:23:41 2022 +0200
    
        will
    
    commit 763dbfd37f3fd8e101e903bceb4b335b165c5f02
    Author: FestimsRepo <102750865+FestimP@users.noreply.github.com>
    Date:   Tue Apr 19 00:50:34 2022 +0200
    
        ..
    
    commit 6afbbb17bbdf7be4343cee42f26344e2d86afdff
    Author: FestimsRepo <102750865+FestimP@users.noreply.github.com>
    Date:   Tue Apr 19 00:39:09 2022 +0200
    
        hmm
    
    commit fb16a6f310bc2b039a807d5a54cd179e2da20521
    Author: FestimsRepo <102750865+FestimP@users.noreply.github.com>
    Date:   Tue Apr 19 00:35:47 2022 +0200
    
        kkull
    
    commit 7d525831e1f44cecb6bb9eee9f8d87bebb03daed
    Author: FestimsRepo <102750865+FestimP@users.noreply.github.com>
    Date:   Tue Apr 19 00:28:09 2022 +0200
    
        sus
    
    commit 42ec1a60b1a1ba7d411092250e178deccb1e03fa
    Author: FestimsRepo <102750865+FestimP@users.noreply.github.com>
    Date:   Tue Apr 19 00:17:30 2022 +0200
    
        sus
    
    commit 98e17359bf98448b8856c84242832a8b7b15b0d7
    Author: FestimsRepo <102750865+FestimP@users.noreply.github.com>
    Date:   Mon Apr 18 23:59:43 2022 +0200
    
        will
    
    commit 4c78f651e6bf06dbba3a78fbcbc37cc2e7c5b600
    Author: FestimsRepo <102750865+FestimP@users.noreply.github.com>
    Date:   Thu Apr 7 23:03:41 2022 +0200
    
        all in
    
    commit 7ea455883a3ba21032db1092bb07190e0411741f
    Author: FestimsRepo <102750865+FestimP@users.noreply.github.com>
    Date:   Thu Apr 7 22:28:19 2022 +0200
    
        aa
    
    commit ff7b5e3c11c3f47f201eb6256cfec1d8e013b039
    Author: FestimsRepo <102750865+FestimP@users.noreply.github.com>
    Date:   Thu Apr 7 22:05:21 2022 +0200
    
        projekt start

---
+++ base.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "100vw";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
